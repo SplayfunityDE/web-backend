7a5b25a378c650963231df726a57022bf9f31e14
Splayfery <splayfery@gmail.com>
2025-06-16 21:05:32 +0200

    create basic spring boot structure

---
+++ src/test/java/de/splayfer/web_backend/WebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.splayfer.web_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
