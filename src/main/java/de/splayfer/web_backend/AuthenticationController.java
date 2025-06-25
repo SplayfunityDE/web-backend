6df1527c63542ec5db2d74ab28d782a8c4c78d6b
Splayfery <splayfery@gmail.com>
2025-06-25 14:30:58 +0200

    remove unused dependencies

--- src/main/java/de/splayfer/web_backend/AuthenticationController.java
+++ src/main/java/de/splayfer/web_backend/AuthenticationController.java
@@ -42,4 +42,6 @@ public class AuthenticationController {
         }
         return hexString.toString();
     }
+
+
 }
--- src/main/java/de/splayfer/web_backend/WebBackendApplication.java
+++ src/main/java/de/splayfer/web_backend/WebBackendApplication.java
@@ -1,10 +1,7 @@
 package de.splayfer.web_backend;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.File;
 
 @SpringBootApplication
 public class WebBackendApplication {
