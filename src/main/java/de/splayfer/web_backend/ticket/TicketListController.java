aab2d5963105e27485f71f780e439b2e2dc5cdd7
splayfery <simon.liedlc@gmail.com>
2025-07-15 17:23:36 +0200

    add /archive endpoint & simplify code

--- src/main/java/de/splayfer/web_backend/ticket/TicketListController.java
+++ src/main/java/de/splayfer/web_backend/ticket/TicketListController.java
@@ -18,19 +18,23 @@ public class TicketListController {
 
     @GetMapping("/all")
     public ResponseEntity<?> listAllTickets() {
-        MongoDBDatabase mongoDBDatabase = MongoDBDatabase.getDatabase("splayfunity");
-        return ResponseEntity.ok("[" +
-                StreamSupport.stream(mongoDBDatabase.findAll("ticket").spliterator(), false)
-                .map(doc -> doc.toJson(JsonWriterSettings.builder().indent(true).build()))
-                .collect(Collectors.joining(",")) +
-                "]");
+        return getCollectionAsResponseEntity("ticket");
     }
 
     @GetMapping("/closed")
     public  ResponseEntity<?> listClosedTickets() {
+        return getCollectionAsResponseEntity("ticket-closed");
+    }
+
+    @GetMapping("/archive")
+    public ResponseEntity<?> listArchivedTickets() {
+        return getCollectionAsResponseEntity("ticket-archive");
+    }
+
+    public ResponseEntity<?> getCollectionAsResponseEntity(String collection) {
         MongoDBDatabase mongoDBDatabase = MongoDBDatabase.getDatabase("splayfunity");
         return ResponseEntity.ok("[" +
-                StreamSupport.stream(mongoDBDatabase.findAll("ticket-closed").spliterator(), false)
+                StreamSupport.stream(mongoDBDatabase.findAll(collection).spliterator(), false)
                         .map(doc -> doc.toJson(JsonWriterSettings.builder().indent(true).build()))
                         .collect(Collectors.joining(",")) +
                 "]");
