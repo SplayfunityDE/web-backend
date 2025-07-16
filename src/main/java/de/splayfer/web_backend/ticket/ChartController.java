a1adf3a9f487b5f4c41359d8dd64b32e2d19cdd6
Splayfery <splayfery@gmail.com>
2025-07-16 20:42:07 +0200

    implement activity chart

--- src/main/java/de/splayfer/web_backend/ticket/ChartController.java
+++ src/main/java/de/splayfer/web_backend/ticket/ChartController.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 
 @RestController
@@ -37,6 +43,25 @@ public class ChartController {
                 statusMap.put("offen", statusMap.getOrDefault("offen", 0) + 1);
         }
         return ResponseEntity.ok(new ObjectMapper().writeValueAsString(statusMap));
+    }
+
+    @GetMapping("/activity")
+    public ResponseEntity<?> getActivityChartData() throws JsonProcessingException {
+        MongoDBDatabase mongoDBDatabase = MongoDBDatabase.getDatabase("splayfunity");
+        HashMap<String, Integer> activityMap = new HashMap<>();
+        LocalDateTime currentDate = LocalDateTime.now();
+        for (Document doc : mongoDBDatabase.findAll("ticket")) {
+            LocalDateTime date = LocalDateTime.ofInstant(doc.getDate("createDate").toInstant(), ZoneId.systemDefault());
+            if (date.isAfter(currentDate.minusDays(1))) //tickets created in the last 24 hours mit not be included in minusDay(1)
+                activityMap.put(String.valueOf(0), activityMap.getOrDefault(String.valueOf(0), 0) + 1);
+            for (int i = 0; i < 6; i++) {
+                if (date.isBefore(currentDate.minusDays(7 * i)) && date.isAfter(currentDate.minusDays(7 * (i + 1)))) {
+                    activityMap.put(String.valueOf(i), activityMap.getOrDefault(String.valueOf(i), 0) + 1);
+                    break;
+                }
+            }
+        }
+        return ResponseEntity.ok(new ObjectMapper().writeValueAsString(activityMap));
     }
 
 }
