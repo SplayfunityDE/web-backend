39c2751e39a643509db374fb26375295d5cf0057
Splayfer <splayfery@gmail.com>
2025-07-16 22:05:58 +0200

    rename count rest path

--- src/main/java/de/splayfer/web_backend/ticket/TicketCountController.java
+++ src/main/java/de/splayfer/web_backend/ticket/TicketCountController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/ticket/stats")
+@RequestMapping("/ticket/count")
 public class TicketCountController {
 
     @GetMapping("/open")
